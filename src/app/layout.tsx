0c678ab6c3adf4c1a519e5dd7eaf8db9041de6aa
Iced Mist <talk2icedmist@gmail.com>
2026-01-15 12:09:31 +0000

    fix incomplete grid and not showing site's logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/logo.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
