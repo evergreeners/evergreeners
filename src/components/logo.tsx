4e4d628d66a4efa33eb698ef782a043205341c05
Iced Mist <talk2icedmist@gmail.com>
2026-01-15 09:49:33 +0000

    FIX: dark green to parent color

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,20 +10,7 @@ export function Logo({ className }: { className?: string }) {
       xmlns="http://www.w3.org/2000/svg"
       className={cn('text-primary', className)}
     >
-      <defs>
-        <linearGradient
-          id="paint0_linear_1_2"
-          x1="0"
-          y1="0"
-          x2="36"
-          y2="36"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="currentColor" stopOpacity="1" />
-          <stop offset="1" stopColor="currentColor" stopOpacity="0.4" />
-        </linearGradient>
-      </defs>
-      <rect width="36" height="36" fill="url(#paint0_linear_1_2)" mask="url(#mask0_1_2)" />
+      <rect width="36" height="36" fill="currentColor" mask="url(#mask0_1_2)" />
       <mask id="mask0_1_2">
         <g fill="white">
           <rect x="0" y="0" width="5" height="5" />
